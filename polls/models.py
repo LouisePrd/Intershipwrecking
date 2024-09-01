02ef3a99aa2408ebc13f4072589d350205c8af24
LouisePrd <louise.peridy22@gmail.com>
2024-09-01 21:33:09 +0200

    add new migration for application object + admin options

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    company = models.CharField(max_length=200)
+    description = models.TextField()
+    applied_at = models.DateTimeField(auto_now_add=True)
+    type_of_application = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    link = models.URLField()
